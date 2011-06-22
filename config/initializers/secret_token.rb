91fbab6fe85079eed92c1eb8cff52cc81a3ad115
Chris Ogden <cogden@pivcon.com>
2011-06-22 10:50:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f13aeca791cffa456ae88cfbc987976592a09aed3bee5f46cc5b01799e2704ed006668ebcc2d1a0d9f3bdeec3e5860d556ea5f90a0530c4d2d88cbd4029da159'
